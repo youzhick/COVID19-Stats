ccdb12e58411d000c72e5aeb50d9ef541734d7e2
Mikhail Gribkov <youzhick@gmail.com>
2020-04-19 14:14:13 +0300

    Uploaded project files

---
+++ dataloader.py
@@ -0,0 +1,261 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import re
+import os
+import requests as req
+import subprocess as sp
+############################################################
+# Data sources
+# https://github.com/CSSEGISandData/COVID-19
+populationSourceURL = 'https://www.worldometers.info/world-population/population-by-country/'
+############################################################
+# Settings
+populationFileName = 'Data/population/population.html'
+casesFileName = 'Data/cases/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
+deathsFileName = 'Data/cases/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv'
+recoveredFileName = 'Data/cases/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv'
+casesGitDirName = 'Data/cases'
+
+# Coming from cases data SCVs:
+countryColumnName = 'Country/Region'
+
+nameReps = [
+    ('United States', 'US'),
+    ('Guam', 'US'),
+    ('DR Congo', 'Congo'),
+    ('Congo (Kinshasa)', 'Congo'),
+    ('Burma', 'Myanmar'),
+    ('Korea, South', 'South Korea'),
+    ('C&ocirc;te d\'Ivoire', 'Cote d\'Ivoire'),
+    ('Taiwan*', 'Taiwan'),
+    ('Czech Republic (Czechia)', 'Czechia'),
+    ('Hong Kong', 'China'),
+    ('Kosovo', 'Serbia'),
+    ('MS Zaandam', 'Netherlands'),
+    ('Sint Maarten', 'Netherlands'),
+    ('Aruba', 'Netherlands'),
+    ('Cura&ccedil;ao', 'Netherlands'),
+    ('Caribbean Netherlands', 'Netherlands'),
+    ('Saint Kitts &amp; Nevis', 'Saint Kitts and Nevis'),
+    ('St. Vincent &amp; Grenadines', 'Saint Vincent and the Grenadines'),
+    ('Anguilla', 'United Kingdom'),
+    ('Gibraltar', 'United Kingdom'),
+    ('Gibraltar', 'United Kingdom'),
+    ('Cayman Islands', 'United Kingdom'),
+    ('Montserrat', 'United Kingdom'),
+    ('Isle of Man', 'United Kingdom'),
+    ('Saint Helena', 'United Kingdom'),
+    ('British Virgin Islands', 'United Kingdom'),
+    ('Turks and Caicos', 'United Kingdom'),
+    ('Bermuda', 'United Kingdom'),
+    ('Cook Islands', 'New Zealand'),
+    ('Tokelau', 'New Zealand'),
+    ('Niue', 'New Zealand'),
+    ('Faeroe Islands', 'Denmark'),
+    ('Greenland', 'Denmark'),
+    ('French Guiana', 'France'),
+    ('Guadeloupe', 'France'),
+    ('New Caledonia', 'France'),
+    ('French Polynesia', 'France'),
+    ('Martinique', 'France'),
+    ('Wallis &amp; Futuna', 'France'),
+    ('Mayotte', 'France'),
+    ('Macao', 'China'),
+    ('Marshall Islands', 'US'),
+    ('Puerto Rico', 'US'),
+    ('R&eacute;union', 'France'),
+    ('Saint Martin', 'France'),
+    ('Saint Pierre &amp; Miquelon', 'France'),
+    ('Saint Barthelemy', 'Netherlands'),
+    ('U.S. Virgin Islands', 'US'),
+    ('Western Sahara', 'Morocco'),
+    ('Sao Tome &amp; Principe', 'Sao Tome and Principe')
+]
+
+dropNames = [
+    'Yemen', 'North Korea', 'Malawi', 'South Sudan', 'Tajikistan',
+    'American Samoa', 'Diamond Princess', 'West Bank and Gaza', 'Comoros',
+    'Falkland Islands', 'Channel Islands', 'Kiribati', 'Samoa',
+    'Lesotho', 'Micronesia', 'Nauru', 'Northern Mariana Islands', 'Palau',
+    'Solomon Islands', 'State of Palestine',
+    'Tonga', 'Turkmenistan', 'Tuvalu', 'Vanuatu'
+]
+############################################################
+def sysExec(cmd, print_stdout=False, stripped=True):
+    p = sp.Popen(cmd, shell=True, stdout=sp.PIPE, stderr=sp.PIPE)    
+    
+    result = []
+
+    while True:
+        s = p.stdout.readline()
+        if not s:
+            s = p.stderr.readline()
+        if not s:
+            break
+        s = s.decode('utf-8', 'ignore') if not stripped else s.decode('utf-8', 'ignore').strip()
+        result.append(s)
+        if print_stdout:
+            print(s, flush=True)
+    
+    return result
+############################################################
+def updateGit():
+    print ('Updating git repository: ' + casesGitDirName + '...')
+    sysExec('git -C ' + casesGitDirName +' pull', print_stdout=True)
+    print('Repository updated.')
+############################################################
+def dropByName(df, name):
+    ind = df[df['name'] == name].index
+    df.drop(ind, inplace=True)
+############################################################
+def renameCountries(df):
+    for name in nameReps:
+        df.replace(name[0], name[1], inplace=True)
+############################################################
+def parsePopLine(str):
+    cells = re.findall(r'<td.*?</td', str)
+    name = re.findall(r'/">.*?</a', cells[1])[0][3:-3].strip()
+    population = int(re.findall(r'>.*?<', cells[2])[0][1:-1].strip().replace(',', ''))
+    landAreaKm2 = int(re.findall(r'>.*?<', cells[6])[0][1:-1].strip().replace(',', ''))
+    urbanPercent = re.findall(r'>.*?%', cells[10])
+    urbanPercent = int(0 if len(urbanPercent) < 1 else urbanPercent[0][1:-1].strip())
+    
+    return {'name': name, 'pop': population, 'area': landAreaKm2, 'urban': urbanPercent}
+# ---------
+def loadPopulation():
+    if not os.path.exists(populationFileName):
+        print('No population file found. Trying to update from WorldMeter...')
+        contents = req.get(populationSourceURL)
+        if not contents.ok:
+            print ('Cannot access WorldMeter.')
+            print ('Check your internet connection and try to manually get ', populationSourceURL)
+            print ('If succeeded, save it to ', populationFileName)
+            return None
+        try:
+            dirname = os.path.dirname(populationFileName)
+            if not os.path.exists(dirname):
+                os.makedirs(dirname)
+            with open(populationFileName, 'w', encoding='utf-8') as f:
+                f.write(contents.text)
+        except:
+            print ('Error saving population data file')
+            return None
+        
+    try:
+        popList=[]
+        with open(populationFileName, 'r') as f:
+            s = f.read()
+            # Here's only one table in the file
+            for l in re.findall(r'<tr.*?</tr', s)[1:]:
+                popList.append(parsePopLine(l))
+                
+        result = pd.DataFrame(popList)
+        renameCountries(result)
+
+        result = result.groupby('name', as_index=False).sum()
+
+        result = result.append(result.sum(numeric_only=True), ignore_index=True)
+        result.iloc[-1, 0] = 'World'
+
+    except:
+        print ('Error loading population data')
+        result = None
+        
+    return result
+############################################################
+def loadCases():
+    if not os.path.exists(casesFileName) or not os.path.exists(deathsFileName) or not os.path.exists(recoveredFileName):
+        print('Cannot find cases data. Have you forgot to update submodules?')
+        return None, None, None
+
+    try:
+        cases = pd.read_csv(casesFileName).drop(['Lat', 'Long'], axis=1)
+        deaths = pd.read_csv(deathsFileName).drop(['Lat', 'Long'], axis=1)
+        recovered = pd.read_csv(recoveredFileName).drop(['Lat', 'Long'], axis=1)
+        
+        renameCountries(cases)
+        renameCountries(deaths)
+        renameCountries(recovered)
+        
+        cases = cases.groupby(countryColumnName, as_index=False).sum()
+        deaths = deaths.groupby(countryColumnName, as_index=False).sum()
+        recovered = recovered.groupby(countryColumnName, as_index=False).sum()
+        
+        cases.rename(columns={countryColumnName: 'name'}, inplace=True);
+        deaths.rename(columns={countryColumnName: 'name'}, inplace=True);
+        recovered.rename(columns={countryColumnName: 'name'}, inplace=True);
+        
+        # They have two Congos with the same data. Remove one
+        dropByName(cases, 'Congo (Brazzaville)')
+        dropByName(deaths, 'Congo (Brazzaville)')
+        dropByName(recovered, 'Congo (Brazzaville)')
+
+        
+        cases = cases.append(cases.sum(numeric_only=True), ignore_index=True)
+        cases.iloc[-1, 0] = 'World'
+        
+        deaths = deaths.append(deaths.sum(numeric_only=True), ignore_index=True)
+        deaths.iloc[-1, 0] = 'World'
+        
+        recovered = recovered.append(recovered.sum(numeric_only=True), ignore_index=True)
+        recovered.iloc[-1, 0] = 'World'
+    except:
+        print ('Error loading cases data. ')
+        cases = None
+        deaths = None
+        recovered = None
+        
+    return cases, deaths, recovered
+############################################################
+def loadData():
+    pop = loadPopulation()
+    cases, deaths, recovered = loadCases()
+    
+    return cases, deaths, recovered, pop
+############################################################
+def normalize(cases, deaths, recovered, pop):
+        
+    for name in dropNames:
+        dropByName(pop, name)
+        dropByName(cases, name)
+        dropByName(deaths, name)
+        dropByName(recovered, name)
+    
+    # We'll not make inner join method to identify possible future problens
+    cases = pd.merge(pop, cases, how='outer')
+    deaths = pd.merge(pop, deaths, how='outer')
+    recovered = pd.merge(pop, recovered, how='outer')
+    
+    failCount = cases[cases.isna().any(axis=1)].shape[0]
+    failCount += deaths[deaths.isna().any(axis=1)].shape[0]
+    failCount += recovered[recovered.isna().any(axis=1)].shape[0]
+    
+    if failCount > 0:
+        print('WARNING: some countries data is inconsistent. Check names merging.')
+    
+    #print (pop)
+    #print(cases[cases.isna().any(axis=1)])
+    #print (res.loc[res['name']=='French Guiana'].iloc[0,:5])
+    
+    return cases, deaths, recovered
+############################################################
+def loadAll(doUpdateGit=True):
+    if doUpdateGit:
+        updateGit()
+    
+    print('Loading data...')
+    cases, deaths, recovered, pop = loadData()
+    
+    res = normalize(cases, deaths, recovered, pop)
+    
+    print('Loaded')
+    return res
+############################################################
+if __name__ == '__main__':
+    print('Testing')
+
+    a = loadAll()
+    cases = a[0]
+    print (cases.loc[cases['name']=='United Kingdom'])
+
+    print('Done')
---
+++ ratings.py
@@ -0,0 +1,267 @@
+# -*- coding: utf-8 -*-
+from dataloader import loadAll;
+import pandas as pd
+import os
+#import matplotlib as mpl
+#mpl.use('TkAgg')
+#mpl.use('Qt4agg')
+import matplotlib.pyplot as plt
+import seaborn; seaborn.set()
+############################################################
+# Settings
+graphsDirName = 'OutPics'
+populationRelationScale = 100000
+smoothRadius = 1
+############################################################
+smoothWindow=str(int(smoothRadius)*2 + 1)
+columnReplaces = {
+    'pop': 'Population',
+    'deaths': 'Deaths',
+    'recovered': 'Recovered',
+    'cases': 'Total cases',
+    'area': 'Area',
+    'urban': 'Urban percentage',
+    
+    'dCases': 'New Cases',
+    'dRecovered': 'New Recovered',
+    'dDeaths': 'New Deaths',
+    
+    'deathsPerRec': 'Deaths per Recovered',
+    'deathsPerPop': 'Deaths per Population',
+    'casesPerPop': 'Cases per Population',
+    
+    'dCasesSmoothed': 'New Cases (' + smoothWindow + '-averaged)',
+    'dRecoveredSmoothed': 'New Recovered (' + smoothWindow + '-averaged)',
+    'dDeathsSmoothed': 'New Deaths (' + smoothWindow + '-averaged)',
+
+    'deathsPerRecSmoothed': 'Deaths per Recovered (' + smoothWindow + '-averaged)',
+    'deathsPerPopSmoothed': 'Deaths per Population (' + smoothWindow + '-averaged)',
+    'casesPerPopSmoothed': 'Cases per Population (' + smoothWindow + '-averaged)'
+}
+############################################################
+def ensureDirsCreated():
+    if not os.path.exists(graphsDirName):
+        os.makedirs(graphsDirName)
+############################################################
+def column2Column(c):
+    return c if not c in columnReplaces.keys() else columnReplaces[c]
+############################################################
+def showRating(data, howMany, column, relTo=None, scale=None, doPaint=True, doPrint=False, doSave=False):
+    
+    if relTo is None or (relTo == 'pop' and scale is None):
+        rData = ratingPrimary(data, column, relative=(relTo == 'pop'))
+        if scale is None:
+            scale = populationRelationScale
+    else:
+        if scale is None or scale <= 0:
+            scale = 1
+        rData = ratingRelation(data, column, relTo, scale=scale)
+
+    title = column2Column(column)
+    if relTo is not None:
+        title = title + ' to ' + ((str(scale) + ' of ') if scale != 1 else '') + column2Column(relTo)
+        
+    showTop(rData, howMany, title=title, doPaint=doPaint, doPrint=doPrint, doSave=doSave)
+############################################################
+def showTop(inp, howMany, title='', doPaint=True, doPrint=False, doSave=False):
+    data = inp[0].iloc[:howMany] 
+    date = date2date(inp[1], addYear=True)
+    titleFull = title + " at " + date
+    if len(title) < 1:
+        title = 'untitled'
+    if doPrint:
+        print (titleFull)
+        print(data)
+        
+    if not doPaint and not doSave:
+        return
+    
+    yMarks = data['name']
+    fig,ax = plt.subplots(1, 1, figsize=(10, howMany/2))
+    ax.invert_yaxis()
+    ax.barh(yMarks, data['total'], color='#00AA99')
+    ax.set_title(titleFull, fontsize=15)
+    xoffs = data['total'][1]*0.015
+    for i, v in enumerate(data['total']):
+        ax.text(v + xoffs, i + .16, str(int(v)), color='black', fontweight='bold')
+    lim = ax.get_xlim()
+    ax.set_xlim((lim[0], lim[1]*1.08))
+
+    if doSave:
+        ensureDirsCreated()
+        fig.savefig(graphsDirName + '/' + title + '.png', dpi=100)
+        
+    #if doPaint:
+        #fig.show()
+############################################################
+months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+def date2date(s, addYear=False):
+    spl = s.split('/')
+    month = months[int(spl[0]) - 1]
+    day = int(spl[1])
+    year = int('20' + spl[2])
+    
+    if addYear:
+        strRep = '%d %s %d' % (day, month, year)
+    else:
+        strRep = '%s %d' % (month, day)
+        
+    return strRep
+############################################################
+def isDispDate(dStr):
+    return dStr.endswith(' 1') or dStr.endswith(' 15')
+############################################################
+def paintList(data, column, relToCases=False, title=None, doPaint=True, doSave=False):
+    fig,ax = plt.subplots(1, 1, figsize=(10, 5))
+    #fig,ax = plt.subplots(1, 1)
+    ticks = [date2date(s) for s in data[0][1].index.to_list()]
+    for country in data:
+        ax.plot(country[1]['cases'] if relToCases else ticks, country[1][column], label=country[0])
+    ax.legend()
+    title = title if title is not None else (column2Column(column) + (' to Total Cases' if relToCases else ''))
+    ax.set_title(title, fontsize=15)
+    
+    fig.canvas.draw()
+    
+    ax.tick_params(axis='x', rotation=45)
+    if not relToCases:
+        for label in ax.get_xticklabels():
+            if  not isDispDate(label.get_text()):
+                label.set_visible(False)
+    if doSave:
+        ensureDirsCreated()
+        fig.savefig(graphsDirName + '/' + title + '.png', dpi=100)
+        
+    #if doPaint:
+        #fig.show()
+############################################################
+def ratingPrimary(data, column, relative=False):
+    dfT = data[0].copy()
+    dfT['total'] = dfT[column] if not relative else dfT[column]/dfT['pop']*populationRelationScale
+    res = dfT[['name', 'total']].sort_values('total', ascending=False).reset_index(drop=True)[0 if relative else 1:]
+    return res, data[1]
+############################################################
+def ratingRelation(data, nom, denom, scale):
+    df = data[0]
+    # Throwing away zero-denominated countries. Most probably it's some
+    # small uninteresting islands os something like that
+    dfT = df[df[denom] != 0].copy()
+    
+    dfT['total'] = dfT[nom]/dfT[denom]*scale
+    res = dfT[['name', 'total']].sort_values('total', ascending=False).reset_index(drop=True)[0:]
+    return res, data[1]
+############################################################
+def mergeForDate(data, date=None):
+    cases = data[0]
+    deaths = data[1]
+    recovered = data[2]
+
+    if date == None:
+        date = cases.columns[-1]
+        
+    casesT = cases[['name', 'pop', 'area', 'urban', date]].copy()
+    casesT.rename(columns={date: 'cases'}, inplace=True);
+        
+    deathsT = deaths[['name', date]].copy()
+    deathsT.rename(columns={date: 'deaths'}, inplace=True);
+        
+    recoveredT = recovered[['name', date]].copy()
+    recoveredT.rename(columns={date: 'recovered'}, inplace=True);
+    
+    res = pd.merge(pd.merge(casesT, deathsT), recoveredT)
+
+    return res, date
+############################################################
+def addSmoothed(df, column, radius):
+    radius = int(radius)
+
+    newCol = column + 'Smoothed'
+
+    rows = df.shape[0]
+    
+    df[newCol] = df[column]
+    for pos in range(rows):
+        cnt = 1
+        for i in range(1, radius + 1):
+            if pos >= i:
+                df[newCol][pos] += df[column][pos - i]
+                cnt += 1
+            if pos <= rows - i - 1:
+                df[newCol][pos] += df[column][pos + i]
+                cnt += 1
+        df[newCol][pos] /= cnt
+############################################################
+def mergeForCountry(data, name='World'):
+    cases = data[0]
+    deaths = data[1]
+    recovered = data[2]
+    
+    casesT = cases[cases['name'] == name].drop(columns=['name', 'pop', 'area', 'urban'])
+    deathsT = deaths[deaths['name'] == name].drop(columns=['name', 'pop', 'area', 'urban'])
+    recoveredT = recovered[recovered['name'] == name].drop(columns=['name', 'pop', 'area', 'urban'])
+    
+    casesT = casesT.transpose()
+    casesT.rename(columns={casesT.columns[0]: 'cases'}, inplace=True)
+    
+    deathsT = deathsT.transpose()
+    deathsT.rename(columns={deathsT.columns[0]: 'deaths'}, inplace=True)
+    
+    recoveredT = recoveredT.transpose()
+    recoveredT.rename(columns={recoveredT.columns[0]: 'recovered'}, inplace=True)
+
+    res = pd.merge(pd.merge(casesT, deathsT, left_index=True, right_index=True), recoveredT, left_index=True, right_index=True)
+    
+    res['pop'] = cases[cases['name']==name]['pop'].iloc[0]
+    res['area'] = cases[cases['name']==name]['area'].iloc[0]
+    res['urban'] = cases[cases['name']==name]['urban'].iloc[0]
+    
+    res['deathsPerRec'] = res['deaths']/res['recovered']
+    res['deathsPerPop'] = res['deaths']/res['pop']
+    res['casesPerPop'] = res['cases']/res['pop']
+    
+    res['dCases'] = res['cases'] - res['cases'].shift(1)
+    res['dDeaths'] = res['deaths'] - res['deaths'].shift(1)
+    res['dRecovered'] = res['recovered'] - res['recovered'].shift(1)
+
+
+    addSmoothed(res, 'dCases', smoothRadius)
+    addSmoothed(res, 'dDeaths', smoothRadius)
+    addSmoothed(res, 'dRecovered', smoothRadius)
+
+    addSmoothed(res, 'deathsPerRec', smoothRadius)
+    addSmoothed(res, 'deathsPerPop', smoothRadius)
+    addSmoothed(res, 'casesPerPop', smoothRadius)
+    
+    return res[1:]
+############################################################
+def mergeForCountriesList(data, names=['World'], starting_date=None):
+    if len(names) < 1:
+        return []
+    
+    return [(n, mergeForCountry(data, n) if starting_date is None else mergeForCountry(data, n).loc[starting_date:]) for n in names]
+############################################################
+if __name__ == '__main__':
+    print('Testing')
+    
+    data = loadAll(doUpdateGit=True)
+    #data = loadAll(doUpdateGit=False)
+
+    #today = mergeForDate(data)
+    #showRating(today, 10, 'deaths', relTo='area', doSave=True)
+    #showRating(today, 10, 'recovered', relTo='pop')
+    
+    
+    
+    startingDate = None
+    startingDate = '3/1/20'
+    countries=['Russia', 'Italy', 'Spain', 'US', 'Germany']
+    #countries=['China']
+    data = mergeForCountriesList(data, names=countries, starting_date=startingDate)
+    paintList(data, 'dCasesSmoothed', relToCases=False)
+    #paintList(data, 'dDeathsSmoothed', relToCases=False)
+    #paintList(data, 'deathsPerPopSmoothed', relToCases=True)
+    
+    from dataloader import sysExec
+    sysExec('dir', print_stdout=True)
+    
+    print('Done.')
